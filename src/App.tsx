fb01efb53f5e9e9b0581e21ac9e89414673aa628
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-11-10 09:55:04 -0800

    add initial code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { Suspense } from 'preact/compat'
-import UserCount from 'components/UserCount'
-
 export default function () {
   return (
     <div className="container mx-auto max-w-prose p-10 prose">
-      <h1>Frontend template</h1>
-      <Suspense fallback={<p>Loading...</p>}>
-        <UserCount />
-      </Suspense>
+      <h1>Will Robinson bot</h1>
     </div>
   )
 }
