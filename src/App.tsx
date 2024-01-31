07125c34a3a73673f986ccc3524bc4429adac8df
Nikita Kolmogorov <backmeupplz@gmail.com>
2024-01-31 09:43:52 -0800

    Update VSCode settings and add warning message

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useAccount } from 'wagmi'
 import { useEffect } from 'preact/hooks'
 import { useSetAtom } from 'jotai'
 import Description from 'components/Description'
+import Link from 'components/Link'
 import Login from 'components/Login'
 import SignerUUID from 'components/SignerUUID'
 import Support from 'components/Support'
@@ -41,6 +42,25 @@ function Main() {
   return (
     <div>
       <Title />
+      <div role="alert" class="alert alert-error">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="stroke-current shrink-0 h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+          />
+        </svg>
+        <span>
+          Warning: Will Robinson bot was shut down! You will get penalized by
+          the algorithm if you auto-like all casts.
+        </span>
+      </div>
       {!isConnected && <Description />}
       <Login />
       {isConnected && <SignerUUID />}
